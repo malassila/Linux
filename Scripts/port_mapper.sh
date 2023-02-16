4b2b713bbf7341fc7fee41d5f97d71cfcb53c25c
mlassila <mlassila@pcsp.com>
2023-02-16 09:29:27 -0500

    Update port_mapper.sh

--- Scripts/port_mapper.sh
+++ Scripts/port_mapper.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-
 num=1
-
+current_drive=""
 while true; do
-    next_drive=$(ls /dev/disk/by-path | awk '!/-part/ {print}' | sort | head -n1)
-    if [[ -n $next_drive ]]; then
+
+    next_drive=$(ls /dev/disk/by-path | awk '!/-part|pci-0000:00:11.0-ata-6/ {print}' | sort | head -n1)
+    
+    # If the next drive is not null and different from the current drive, assign it to the next port
+    if [[ -n $next_drive ]] && [[ $next_drive != $current_drive ]]; then
+        echo "Next drive: $next_drive"
+        current_drive=$next_drive
         echo "Drive $next_drive detected, assigning to port$num"
-        touch "/DRIVE/Port/port$num"
-        chmod 777 "/DRIVE/Port/port$num"
+            if [[ -f "/DRIVE/Port/port$num" ]]; then
+                rm "/DRIVE/Port/port$num"
+            fi
         ln -s "/dev/disk/by-path/$next_drive" "/DRIVE/Port/port$num"
         port=$((port+1))
     fi
-    sleep 1
+
+    sleep 2
+
 done
